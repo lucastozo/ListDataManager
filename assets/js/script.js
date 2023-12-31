66ad30731840516e874c234ced97026b60554930
Lucas Tozo <lucastozo7@gmail.com>
2023-12-30 22:42:17 -0400

    submit file done

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+document.getElementById('formFile').addEventListener('change', function() {
+    var submitButton = document.getElementById('submit-button');
+    submitButton.disabled = !this.files.length;
+    submitButton.textContent = 'Enviar';
+});
+
+document.getElementById('submit-button').addEventListener('click', function()
+{
+    var submitButton = document.getElementById('submit-button');
+    submitButton.disabled = true;
+    var file = document.getElementById('formFile').files[0];
+    VerificaArquivoValido(file).then(isValid => 
+    {
+        submitButton.textContent = 'Enviado ✅';
+    }).catch(error => 
+    {
+        submitButton.textContent = 'Arquivo Inválido ❌';
+    });
+});
+
+function VerificaArquivoValido(file)
+{
+    return new Promise((resolve, reject) => 
+    {
+        var reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = function() 
+        {
+            try 
+            {
+                var obj = JSON.parse(reader.result);
+                if(obj.TipoData == "level")
+                {
+                    console.log("level");
+                    resolve(true);
+                }
+                else if(obj.TipoData == "record")
+                {
+                    console.log("record");
+                    resolve(true);
+                }
+                else
+                {
+                    throw new Error("Arquivo inválido.");
+                }
+            } 
+            catch (error) 
+            {
+                reject(error);
+            }
+        }
+        reader.onerror = function() 
+        {
+            reject(new Error("Erro ao ler o arquivo."));
+        }
+    });
+}
