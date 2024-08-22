236d05c2e84ce92800cd11c8f85f4a84c7c5b056
Lucas Tozo <lucastozo7@gmail.com>
2024-08-22 18:27:46 -0400

    test

--- api/hello-api.js
+++ api/hello-api.js
@@ -1,8 +1,8 @@
 // test api, ignore it
 module.exports = (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    //res.setHeader('Access-Control-Allow-Origin', '*');
+    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    //res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
