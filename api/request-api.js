2e362de90a501e0cc567de2cd67779b06ed1df2f
Lucas Tozo <lucastozo7@gmail.com>
2024-08-23 01:11:37 -0400

    Update request-api.js

--- api/request-api.js
+++ api/request-api.js
@@ -76,6 +76,7 @@ module.exports = async (req, res) => {
         });
         return res.status(200).json({ message: 'Request sent successfully' });
     } catch (error) {
+        console.error(error);
         return res.status(500).json({ message: 'Internal Server Error' });
     }
 }
