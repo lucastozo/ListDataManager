40389f6fe3369fd4d97b76ba7792bcb7b5b82d62
Lucas Tozo <lucastozo7@gmail.com>
2024-08-22 22:24:23 -0400

    fix

--- api/request-api.js
+++ api/request-api.js
@@ -8,7 +8,7 @@
 
 const axios = require('axios');
 module.exports = async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://demonlistbr.online/');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
