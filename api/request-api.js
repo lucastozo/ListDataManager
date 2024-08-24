383e68d810d0ea2ff24d8d1d10fe6f219c0744db
Lucas Tozo <lucastozo7@gmail.com>
2024-08-23 23:15:39 -0400

    ??

--- api/request-api.js
+++ api/request-api.js
@@ -60,8 +60,10 @@
 const axios = require('axios');
 module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'PUT, POST');
+    res.setHeader('Access-Control-Allow-Methods', 'PUT, POST', 'OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') return res.status(200).end();
 
     const CALLING_METHOD = req.method;
     const { object, dataMode } = req.body;
